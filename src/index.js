251c66d7568e8a074537ff1fe6bdac9c603a91f1
joamartico <63528825+joamartico@users.noreply.github.com>
2020-09-30 12:53:26 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
 		</DataLayer>
 	</React.StrictMode>,
 	document.getElementById("root")
-);777
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
